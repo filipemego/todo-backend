afaa53a7788174066da3fbb8bda68b76a582f60c
Filipe Gonçalves <filipemego@dev.lan>
2018-07-29 19:11:05 +0000

    change todo table struct

--- migration/todos.sql
+++ migration/todos.sql
@@ -31,12 +31,14 @@ CREATE TABLE IF NOT EXISTS `todos` (
   `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` text COLLATE utf8mb4_unicode_ci,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `todos` (`id`, `title`, `content`) VALUES
 ('260954e3-8a3c-41d9-87c4-371db1241e83', 'Comprar leite', NULL),
 ('7777266a-bed7-47b2-addb-277cf5cff410', 'Pegar as crianças na escola', 'Não esquecer de pagar a mensalidade também.'),
-('c989d151-e48f-4b88-b8fa-07ec9917f4e7', 'Trocar o chuveiro', '');
+('c989d151-e48f-4b88-b8fa-07ec9917f4e7', 'Trocar o chuveiro', NULL);
 
 COMMIT;
